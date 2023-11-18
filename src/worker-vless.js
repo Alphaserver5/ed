831cc7ee0c9e2ec4e252a9a805211285b3557609
Alphaserver5 <129628328+Alphaserver5@users.noreply.github.com>
2023-11-18 15:01:12 +0330

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -6,7 +6,8 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = '';
+const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 
 if (!isValidUUID(userID)) {
@@ -628,4 +629,3 @@ clash-meta
 ################################################################
 `;
 }
-
